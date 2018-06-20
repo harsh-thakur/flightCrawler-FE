c4f668dd1e5e6f7ff81a8d2994fa7006d81b8529
Shubham Latiyan <shubham.latiyan@venturepact.com>
2018-06-20 14:43:13 +0530

    sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  from: any;
   title = 'app';
+
+
+  constructor(){
+
+  }
+  ngOnInit() {
+    console.log('data', this.from);
+    
+  }
 }
